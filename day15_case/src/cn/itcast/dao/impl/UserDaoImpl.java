df91e990fbe963da87793f665c093e2e0229ab8e
Alexed-star <2631266845@qq.com>
2021-04-28 15:26:51 +0800

    itcast学习记录

--- day15_case/src/cn/itcast/dao/impl/UserDaoImpl.java
+++ day15_case/src/cn/itcast/dao/impl/UserDaoImpl.java
@@ -2,14 +2,19 @@ package cn.itcast.dao.impl;
 
 import cn.itcast.dao.UserDao;
 import cn.itcast.domain.User;
+import cn.itcast.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
-
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
     @Override
     public List<User> findAll() {
         //使用JDBC操作数据库
-        return null;
+        String sql = "select * from user";
+        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<User>(User.class));
+        return users;
     }
 }
